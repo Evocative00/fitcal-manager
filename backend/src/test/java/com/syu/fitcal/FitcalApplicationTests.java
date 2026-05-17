41a43320699a015231846865b6d36dfe71d9f65b
Sang Jun KOH <joony1024@naver.com>
2026-05-17 16:32:19 +0900

    chore: initialize backend project

---
+++ backend/src/test/java/com/syu/fitcal/FitcalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syu.fitcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
